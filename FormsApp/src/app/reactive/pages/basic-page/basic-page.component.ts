4fcf7cfd1a5ecbdd9187c7aa9f33543bdd0eadd7
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-24 20:11:52 -0500

    Vid140-Metodos de ayuda para los errores

--- FormsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ FormsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -7,13 +7,15 @@ const rtx5090 = {
   inStorage: 6,
 }
 
+
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent implements OnInit{
- // public myForm: FormGroup = new FormGroup({
+export class BasicPageComponent implements OnInit {
+
+  // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl('', []),
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
@@ -25,21 +27,48 @@ export class BasicPageComponent implements OnInit{
     inStorage: [0, [ Validators.required, Validators.min(0) ]],
   })
 
-constructor (private fb:FormBuilder){}
+  constructor( private fb: FormBuilder ){}
+
   ngOnInit(): void {
     // this.myForm.reset( rtx5090 );
   }
 
-onSave():void{
-  if ( this.myForm.invalid ){
-    this.myForm.markAllAsTouched();
-    return;
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
   }
-  console.log(this.myForm.value);
 
-this.myForm.reset({price: 0, inStorage: 0});
+  getFieldError( field: string ): string | null {
 
-}
+    if ( !this.myForm.controls[field] ) return null;
 
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+
+  onSave():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+
+  }
 
 }
